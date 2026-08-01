8afac98efa820575f5f41409d179cc4838efe3e2
iKzuu <anangsetiaji30@gmail.com>
2026-08-01 23:25:02 +0700

    refactor: standardize quotes and improve readability in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('mobile-menu-button');
-    const menu = document.getElementById('mobile-menu');
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("mobile-menu-button");
+    const menu = document.getElementById("mobile-menu");
 
     if (btn && menu) {
-        btn.addEventListener('click', () => {
-            if (menu.classList.contains('hidden')) {
-                menu.classList.remove('hidden');
-                menu.classList.add('flex');
+        btn.addEventListener("click", () => {
+            if (menu.classList.contains("hidden")) {
+                menu.classList.remove("hidden");
+                menu.classList.add("flex");
             } else {
-                menu.classList.remove('flex');
-                menu.classList.add('hidden');
+                menu.classList.remove("flex");
+                menu.classList.add("hidden");
             }
         });
     }
+});
+
+
+// expand / collapse FAQ
+document.addEventListener('DOMContentLoaded', () => {
+
+    const faqCards = document.querySelectorAll('.faq-card-container');
+
+    faqCards.forEach(card => {
+        
+        card.addEventListener('click', function() {
+            const content = this.querySelector('.faq-content');
+            const icon = this.querySelector('.faq-icon');
+
+            if (content.style.gridTemplateRows === '1fr') {
+                content.style.gridTemplateRows = '0fr';
+                icon.classList.remove('rotate-180');
+            } else {
+                content.style.gridTemplateRows = '1fr';
+                icon.classList.add('rotate-180');
+            }
+        });
+        
+    });
+    
 });
